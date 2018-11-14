9a79c145db4f97742dff238ebfe02843cf32fa2f
walkerSwift <walker@empirex.tech>
2018-11-14 14:48:37 -0500

    added google api functionality to heroku

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,4 +4,10 @@ module.exports = function(app) {
   app.get("/", function(request, response) {
     response.sendFile(path.join(__dirname, "../public/index.html"));
   });
+
+  app.get("/google706da3544a696cfd.html ", function(request, response) {
+    response.sendFile(
+      path.join(__dirname, "../public/google706da3544a696cfd.html")
+    );
+  });
 };
